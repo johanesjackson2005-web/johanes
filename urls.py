2025b44d28b109b90656ad885658e21c003ce7c8
Johanes Jackson <johanesjackson2005@gmail.com>
2026-05-27 21:35:05 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,30 +1,24 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
+"""
+URL configuration for myapp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('contactus/',views.contactus, name='contactus'),
-    path('slideshow/', views.slideshow, name='slideshow'),
-    path('about/', views.about, name='about'),
-    path(
-        'forgot-password/',
-        views.forgot_password,
-        name='forgot_password'
-    ),
-
-    path(
-        'verify-otp/',
-        views.verify_otp,
-        name='verify_otp'
-    ),
-
-    path(
-        'reset-password/',
-        views.reset_password,
-        name='reset_password'
-    ),
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    
 ]
